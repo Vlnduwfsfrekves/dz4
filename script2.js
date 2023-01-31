380e548aecb0af1038fca85982f76069c827149a
Vlnduwfsfrekves <112093090+Vlnduwfsfrekves@users.noreply.github.com>
2023-01-31 20:23:57 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,5 @@
+for (let index1 = 1; index1 < 11; index1++) {
+    if(index1%2===0){
+        console.log(index1);
+    };
+};
---
+++ script4.js
@@ -0,0 +1,7 @@
+let name1;
+for (let i = 0;i===0;){
+    name1=prompt("Name of superhero");
+    if(name1.length>=6){
+        i++;
+    };
+};
